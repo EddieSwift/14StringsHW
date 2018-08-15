07ca8347723dd2d62b50ad60ad663412b851926f
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-08-15 11:27:19 +0300

    Initial Commit

---
+++ 14StringsHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  14StringsHW
+//
+//  Created by Eduard Galchenko on 8/15/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 14StringsHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  14StringsHW
+//
+//  Created by Eduard Galchenko on 8/15/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
